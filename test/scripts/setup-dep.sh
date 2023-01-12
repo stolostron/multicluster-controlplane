3b19111a10f893136c4d40b6de3b8286ee5592b4
Meng Yan <myan@redhat.com>
2023-01-12 15:55:07 +0800

    fix e2e test (#9)
    * fix e2e test
    
    Signed-off-by: myan <myan@redhat.com>
    
    without move ginkgo dir
    
    Signed-off-by: myan <myan@redhat.com>
    
    check tool
    
    Signed-off-by: myan <myan@redhat.com>
    
    remove sudo
    
    Signed-off-by: myan <myan@redhat.com>
    
    add sudo
    
    Signed-off-by: myan <myan@redhat.com>
    
    sleep for test
    
    Signed-off-by: myan <myan@redhat.com>
    
    add makefile.prow
    
    Signed-off-by: myan <myan@redhat.com>
    
    add prow e2e
    
    Signed-off-by: myan <myan@redhat.com>
    
    remove the debug code
    
    Signed-off-by: myan <myan@redhat.com>
    
    remove the conflict scripts
    
    Signed-off-by: myan <myan@redhat.com>
    
    change dir
    
    Signed-off-by: myan <myan@redhat.com>
    
    change dir
    
    Signed-off-by: myan <myan@redhat.com>
    
    remove docker
    
    Signed-off-by: myan <myan@redhat.com>
    
    install cfssl
    
    Signed-off-by: myan <myan@redhat.com>
    
    upgrade kustomize
    
    Signed-off-by: myan <myan@redhat.com>
    
    given permission to test e2e
    
    Signed-off-by: myan <myan@redhat.com>
    
    sleep for debug
    
    Signed-off-by: myan <myan@redhat.com>
    
    fixed e2e issues
    
    Signed-off-by: myan <myan@redhat.com>
    
    * fixed cluseteradm install
    
    Signed-off-by: myan <myan@redhat.com>
    
    * exec shell with souce
    
    Signed-off-by: myan <myan@redhat.com>
    
    Signed-off-by: myan <myan@redhat.com>

--- test/scripts/setup-dep.sh
+++ test/scripts/setup-dep.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 
-bin_dir="$(go env GOPATH)/bin"
+bin_dir="/usr/bin"
+
+function check_golang() {
+  export PATH=$PATH:/usr/local/go/bin
+  if ! command -v go >/dev/null 2>&1; then
+    wget https://dl.google.com/go/go1.19.3.linux-amd64.tar.gz >/dev/null 2>&1
+    tar -C /usr/local/ -xvf go1.19.3.linux-amd64.tar.gz >/dev/null 2>&1
+    rm go1.19.3.linux-amd64.tar.gz
+  fi
+  if [[ $(go version) < "go version go1.19" ]]; then
+    echo "go version is less than 1.19, update to 1.19"
+    rm -rf /usr/local/go
+    wget https://dl.google.com/go/go1.19.3.linux-amd64.tar.gz >/dev/null 2>&1
+    tar -C /usr/local/ -xvf go1.19.3.linux-amd64.tar.gz >/dev/null 2>&1
+    rm go1.19.3.linux-amd64.tar.gz
+    sleep 2
+  fi
+  echo "go version: $(go version)"
+}
+
+function check_kind() {
+  if ! command -v kind >/dev/null 2>&1; then 
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64 
+    chmod +x ./kind
+    mv ./kind ${bin_dir}/kind
+  fi
+  if [[ $(kind version |awk '{print $2}') < "v0.12.0" ]]; then
+    rm -rf $(which kind)
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64 
+    chmod +x ./kind
+    mv ./kind ${bin_dir}/kind
+  fi
+  echo "kind version: $(kind version)"
+}
 
 function check_kubectl() {
   if ! command -v kubectl >/dev/null 2>&1; then 
@@ -11,7 +44,7 @@ function check_kubectl() {
         curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/darwin/amd64/kubectl
     fi
     chmod +x ./kubectl
-    sudo mv ./kubectl ${bin_dir}/kubectl
+    mv ./kubectl ${bin_dir}/kubectl
   fi
   echo "kubectl version: $(kubectl version --client --short)"
 }
@@ -21,7 +54,8 @@ function check_kustomize() {
     echo "This script will install kustomize on your machine"
     curl -LO "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
     chmod +x ./install_kustomize.sh
-    source ./install_kustomize.sh 3.8.2 ${bin_dir}
+    source ./install_kustomize.sh 4.5.7 ${bin_dir}
+    rm ./install_kustomize.sh
   fi
   echo "kustomize version: $(kustomize version)"
 }
@@ -40,12 +74,30 @@ function check_clusteradm() {
 function check_ginkgo() {
   if ! command -v ginkgo >/dev/null 2>&1; then 
     go install github.com/onsi/ginkgo/v2/ginkgo@v2.5.0
-    sudo mv $(go env GOPATH)/bin/ginkgo ${bin_dir}/ginkgo
+    mv $(go env GOPATH)/bin/ginkgo ${bin_dir}/ginkgo
   fi 
   echo "ginkgo version: $(ginkgo version)"
 }
 
+function check_cfssl() {
+  if ! command -v cfssl >/dev/null 2>&1; then 
+    curl --retry 10 -L -o cfssl https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssl_1.5.0_linux_amd64
+    chmod +x cfssl || true
+    mv cfssl ${bin_dir}/cfssl
+  fi 
+  echo "cfssl path: $(which cfssl)"
+  if ! command -v cfssljson >/dev/null 2>&1; then 
+    curl --retry 10 -L -o cfssljson https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssljson_1.5.0_linux_amd64
+    chmod +x cfssljson || true
+    mv cfssljson ${bin_dir}/cfssljson
+  fi 
+  echo "cfssljson path: $(which cfssljson)"
+}
+
+check_golang
+check_kind
 check_kubectl
 check_kustomize
 check_clusteradm
 check_ginkgo
+check_cfssl
